c8338bb1353278b105768156a2718a6e67019165
SyedKashanAbbasNaqvi <kashan@zikrainfotech.com>
2024-09-14 01:36:44 +0500

    class1

---
+++ bin/OOPSPRACTICEQUESTION/ClassesandObjects/practiceq1.dart
@@ -0,0 +1,14 @@
+// Create a Car class with properties brand, model, and year.
+//Write a constructor to initialize these properties. Then, create an object of this class.
+class Car {
+  String? brand;
+  String? model;
+  String? year;
+  Car(this.brand, this.model, this.year);
+}
+
+void main() {
+  Car audia8 = Car("audi", "Audi A", "200");
+
+  
+}
